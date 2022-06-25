15ae8498c83184a5f0b1800c3ba8b7d1377b8798
Ilkhomjon2604 <m347es@gmail.com>
2022-06-25 11:09:34 +0200

    adding js to features section

--- js/main.js
+++ js/main.js
@@ -1,9 +1,38 @@
-elQuestion = document.querySelectorAll('.qa-wrapper');
-elAnswer = document.querySelector(".answer");
+let elsTabTitle = document.querySelectorAll(".features-list-title");
+let elsTabPanel = document.querySelectorAll(".js-features-item");
+let elsTabLink = document.querySelectorAll(".js-tab-link");
 
-
-for(let i=0; i<= elQuestion; i++){
-elQuestion.addEventListener('click', function(){
-elAnswer.classList.toggle(".asnwer-active")
-})
+function DeactivateTabItem () {
+  elsTabTitle.forEach( function(elTabTitle){
+    elTabTitle.classList.remove("list-title-active")
+  });
 };
+
+function DeactivateTabPanel () {
+  elsTabPanel.forEach( function(elTabPanel){
+    elTabPanel.classList.remove("features-list-item-active")
+  });
+};
+
+elsTabLink.forEach( function (elTabLink) {
+  elTabLink.addEventListener('click', function(evt){
+    // Prevent page move?
+    evt.preventDefault();
+
+    // Remove item ACtive class from features-list-title
+    DeactivateTabItem();
+
+    // Add active class to current item title
+    elTabLink.parentElement.classList.add("list-title-active");
+
+    // Deactivate feature panel
+    DeactivateTabPanel ();
+
+    //Add activation class to current panel
+
+    let elTabTarget = document.querySelector(elTabLink.dataset.tabTarget);
+    elTabTarget.classList.add("features-list-item-active");
+
+
+  });
+});
